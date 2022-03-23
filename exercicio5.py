a9954b59d74b9d2bb57c2aaade4854269cce8c8b
DiogoBalconi <101756111+DiogoBalconi@users.noreply.github.com>
2022-03-23 19:31:22 -0300

    Create exercicio5.py

---
+++ exercicio5.py
@@ -0,0 +1,7 @@
+#Faça um programa em linguagem Python que converta metros para centímetros.
+
+metros = float(input("Informe quantos metros quer converter? "))
+
+centimetros = metros * 100
+
+print(f"{metros} metros são {centimetros} centímetros")
